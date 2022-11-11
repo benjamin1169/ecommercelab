f07f891bbd43ce2f9d294e3b538d3e304a36323d
benjamin1169 <benjamin.nkansah@ashesi.edu.gh>
2022-11-11 18:40:56 +0000

    updated

--- settings/db_credentials.php
+++ settings/db_credentials.php
@@ -1,11 +1,8 @@
-<?php
-define('SERVERNAME','localhost');
-define('HOSTNAME','root');
-define('PASSWORD','');
-define('DATABASE','shoppn');
 
-
-?>
+// define('SERVERNAME','localhost');
+// define('HOSTNAME','root');
+// define('PASSWORD','');
+// define('DATABASE','shoppn');
 
 
 
@@ -16,9 +13,9 @@ define('DATABASE','shoppn');
 // define('DATABASE','shoppn');
 // mysql://b998d05e1b70a9:2f54ddce@us-cdbr-east-06.cleardb.net/heroku_d95779be487eacf?reconnect=true
 // */
-// define('SERVERNAME','us-cdbr-east-06.cleardb.net');
-// define('HOSTNAME','b998d05e1b70a9');
-// define('PASSWORD','2f54ddce');
-// define('DATABASE','heroku_d95779be487eacf');
+define('SERVERNAME','us-cdbr-east-06.cleardb.net');
+define('HOSTNAME','b998d05e1b70a9');
+define('PASSWORD','2f54ddce');
+define('DATABASE','heroku_d95779be487eacf');
 
 ?>
